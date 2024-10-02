05abd10ea9805262b2eda3f6501a1b49933179de
Max <kefirka552@gmail.com>
2024-10-02 21:24:24 +0300

    LabWork#8

---
+++ KASD-Lab#8/KASD-Lab#8/Program.cs
@@ -0,0 +1,55 @@
+﻿using MyVectorLib;
+using System.Reflection.Metadata;
+
+public class Project
+{
+    public class MyStack<T>: MyVector<T>
+    {
+        private MyVector<T> stack;
+        private int top;
+
+        public MyStack()
+        {
+            top = -1;
+            stack = new MyVector<T>(1);
+        }
+        public void Push(T element)
+        {
+            stack.Add(element);
+            top++;
+        }
+        public T Pop()
+        {
+            T element = stack.Get(top);
+            stack.RemoveByIndex(top);
+            top--;
+            return element;
+        }
+        public T Peek()
+        {
+            return stack.Get(top);
+        }
+        public new bool IsEmpty()
+        {
+            return stack.IsEmpty();
+        }
+        public int Search(T item)
+        {
+            if (stack.IndexOf(item) == -1) return -1;
+            return top - stack.IndexOf(item) + 1;
+        }
+        public new void Print()
+        {
+            stack.Print();
+        }
+    }
+    //static void Main(string[] args)
+    //{
+    //    MyStack<int> stack = new MyStack<int>();
+    //    stack.Push(3);
+    //    stack.Push(1);
+    //    stack.Push(7);
+    //    stack.Push(2);
+    //    stack.Print();
+    //}
+}
