def77ef7d2084016ed7697c72219fe2db8d75945
Max <kefirka552@gmail.com>
2024-12-07 20:37:15 +0300

    LabWork#16

---
+++ KASD-Lab#16/KASD-Lab#16/TestAll.cs
@@ -0,0 +1,15 @@
+﻿using MyLinkedLIst;
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] ints = { 1, 2, 3, 4, 5, 6};
+        MyLinkedList<int> list = new MyLinkedList<int>(ints);
+        list.AddAll(ints.Length, [7, 6, 5, 4, 3]);
+        //list.Print();
+        //Console.WriteLine(list.PollLast());
+        list.RemoveFirstOccurrence(3);
+        list.Print();
+    }
+}
