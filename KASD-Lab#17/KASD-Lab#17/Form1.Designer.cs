a7863825bd46cbc6db6b8202a1e5445a7ddfc9b8
Max <kefirka552@gmail.com>
2024-12-08 14:50:43 +0300

    LabWork-#17

---
+++ KASD-Lab#17/KASD-Lab#17/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace KASD_Lab_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.StartTests = new System.Windows.Forms.Button();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(-3, 0);
+            this.zedGraph.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(879, 643);
+            this.zedGraph.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.DimGray;
+            this.comboBox1.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Add",
+            "Get",
+            "Set",
+            "Add (index, value)",
+            "Remove"});
+            this.comboBox1.Location = new System.Drawing.Point(883, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(366, 60);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Операция";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // StartTests
+            // 
+            this.StartTests.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.StartTests.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartTests.Location = new System.Drawing.Point(883, 404);
+            this.StartTests.Name = "StartTests";
+            this.StartTests.Size = new System.Drawing.Size(366, 72);
+            this.StartTests.TabIndex = 2;
+            this.StartTests.Text = "Запуск тестов";
+            this.StartTests.UseVisualStyleBackColor = false;
+            this.StartTests.Click += new System.EventHandler(this.StartTests_Click);
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.BackColor = System.Drawing.Color.RosyBrown;
+            this.DrawButton.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DrawButton.Location = new System.Drawing.Point(883, 482);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(366, 148);
+            this.DrawButton.TabIndex = 3;
+            this.DrawButton.Text = "Отрисовка";
+            this.DrawButton.UseVisualStyleBackColor = false;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(1261, 642);
+            this.Controls.Add(this.DrawButton);
+            this.Controls.Add(this.StartTests);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.zedGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button StartTests;
+        private System.Windows.Forms.Button DrawButton;
+    }
+}
+
---
+++ KASD-Lab#17/KASD-Lab#17/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+using ZedGraph;
+using MyLinkedList;
+using MyListLib;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net.Http.Headers;
+
+namespace KASD_Lab_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GraphPane pane = zedGraph.GraphPane;
+            pane.CurveList.Clear();
+            pane.XAxis.Title.Text = "Размер массива (шт)";
+            pane.YAxis.Title.Text = "Время выполнения (мс)";
+            pane.Title.Text = "Зависимости времени выполнения от размера массива";
+        }
+        int operationIndex = -1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            operationIndex = comboBox1.SelectedIndex;
+        }
+
+        private void StartTests_Click(object sender, EventArgs e)
+        {
+            GraphPane pane = zedGraph.GraphPane;
+            pane.CurveList.Clear();
+            Random random = new Random();
+            PointPairList list1 = new PointPairList();
+            PointPairList list2 = new PointPairList();
+            MyArrayList<int> arrayList = new MyArrayList<int>(10);
+            MyLinkedList<int> linkedList = new MyLinkedList<int>();
+            Stopwatch timer = new Stopwatch();
+            switch (operationIndex)
+            {
+                case -1:
+                    return;
+                case 0:
+                    for (int size = 100; size <= 10000; size *= 10)
+                    {
+                        double sum1 = 0; double sum2 = 0;
+                        for (int i = 0; i < 20; i++)
+                        {
+                            timer.Start();
+                            for (int j = 0; j < size; j++) arrayList.Add(j);
+                            timer.Stop();
+                            sum1 += timer.ElapsedMilliseconds;
+                            timer.Restart();
+                            for (int j = 0; j < size; j++) linkedList.Add(j);
+                            timer.Stop();
+                            sum2 += timer.ElapsedMilliseconds;
+                            timer.Reset();
+                        }
+                        list1.Add(size, sum1 / 20);
+                        list2.Add(size, sum2 / 20);
+                        arrayList.Clear();
+                        linkedList.Clear();
+                    }
+                    break;
+                case 1:
+                    for (int size = 100; size <= 1000; size *= 10)
+                    {
+                        double sum1 = 0; double sum2 = 0;
+                        for(int i = 0; i < 20; i++)
+                        {
+                            for (int j = 0; j < size; j++)arrayList.Add(j);
+                            for (int j = 0; j < size; j++) linkedList.Add(j);
+                            int finding = random.Next(0, size - 1);
+                            timer.Start();
+                            for (int j = 0; j < size; j++) arrayList.Get(finding);
+                            timer.Stop();
+                            sum1 += timer.ElapsedMilliseconds;
+                            timer.Restart();
+                            for (int j = 0; j < size; j++) linkedList.Get(finding);
+                            timer.Stop();
+                            sum2 += timer.ElapsedMilliseconds;
+                            timer.Reset();
+                        }
+                        list1.Add(size, sum1 / 20);
+                        list2.Add(size, sum2 / 20);
+                        arrayList.Clear();
+                        linkedList.Clear();
+                    }
+                    break;
+                case 2:
+                    for (int size = 100; size <= 1000; size *= 10)
+                    {
+                        double sum1 = 0; double sum2 = 0;
+                        for (int i = 0; i < 20; i++)
+                        {
+                            for (int j = 0; j < size; j++) arrayList.Add(j);
+                            for (int j = 0; j < size; j++) linkedList.Add(j);
+                            timer.Start();
+                            for (int j = 0; j < size; j++)
+                            {
+                                int index = random.Next(0, arrayList.Size() - 1);
+                                int number = random.Next(0, 10000);
+                                arrayList.Set(index, number);
+                            }
+                            timer.Stop();
+                            sum1 += timer.ElapsedMilliseconds;
+                            timer.Restart();
+                            for (int j = 0; j < size; j++)
+                            {
+                                int index = random.Next(0, linkedList.Size() - 1);
+                                int number = random.Next(0, 10000);
+                                linkedList.Set(index, number);
+                            }
+                            timer.Stop();
+                            sum2 += timer.ElapsedMilliseconds;
+                            timer.Reset();
+                        }
+                        list1.Add(size, sum1 / 20);
+                        list2.Add(size, sum2 / 20);
+                        arrayList.Clear();
+                        linkedList.Clear();
+                    }
+                    break;
+                case 3:
+                    for (int size = 100; size <= 1000; size *= 10)
+                    {
+                        double sum1 = 0; double sum2 = 0;
+                        for (int i = 0; i < 20; i++)
+                        {
+                            for (int j = 0; j < size; j++) arrayList.Add(j);
+                            for (int j = 0; j < size; j++) linkedList.Add(j);
+                            timer.Start();
+                            for (int j = 0; j < size; j++)
+                            {
+                                int index = random.Next(0, arrayList.Size() - 1);
+                                int number = random.Next(0, 10000);
+                                arrayList.Add(index, number);
+                            }
+                            timer.Stop();
+                            sum1 += timer.ElapsedMilliseconds;
+                            timer.Restart();
+                            for (int j = 0; j < size; j++)
+                            {
+                                int index = random.Next(0, linkedList.Size() - 1);
+                                int number = random.Next(0, 10000);
+                                linkedList.Add(index, number);
+                            }
+                            timer.Stop();
+                            sum2 += timer.ElapsedMilliseconds;
+                            timer.Reset();
+                        }
+                        list1.Add(size, sum1 / 20);
+                        list2.Add(size, sum2 / 20);
+                        arrayList.Clear();
+                        linkedList.Clear();
+                    }
+                    break;
+                case 4:
+                    for (int size = 100; size <= 1000; size *= 10)
+                    {
+                        double sum1 = 0; double sum2 = 0;
+                        for (int i = 0; i < 20; i++)
+                        {
+                            for (int j = 0; j < size; j++) arrayList.Add(j);
+                            for (int j = 0; j < size; j++) linkedList.Add(j);
+                            timer.Start();
+                            for (int j = 0; j < size; j++)
+                            {
+                                int number = random.Next(0, arrayList.Size() + 1);
+                                arrayList.Remove(number);
+                            }
+                            timer.Stop();
+                            sum1 += timer.ElapsedMilliseconds;
+                            timer.Restart();
+                            for (int j = 0; j < size; j++)
+                            {
+                                int number = random.Next(0, linkedList.Size() + 1);
+                                linkedList.Remove(number);
+                            }
+                            timer.Stop();
+                            sum2 += timer.ElapsedMilliseconds;
+                            timer.Reset();
+                        }
+                        list1.Add(size, sum1 / 20);
+                        list2.Add(size, sum2 / 20);
+                        arrayList.Clear();
+                        linkedList.Clear();
+                    }
+                    break;
+            }
+            pane.AddCurve("Массив", list1, Color.Green, SymbolType.Default);
+            pane.AddCurve("Список", list2, Color.Red, SymbolType.Default);
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
