ccf14136350f1d6e926921a8b3985619c0afb248
Max <kefirka552@gmail.com>
2024-12-09 22:29:41 +0300

    LabWork-#20

---
+++ KASD-Lab#20/KASD-Lab#20/MyHashMapLib.cs
@@ -0,0 +1,175 @@
+﻿namespace MyHashMapLib
+{
+    public class MyHashMap<K, V>
+    {
+        private class Entry
+        {
+            public K key { get; set; }
+            public V value { get; set; }
+            public Entry? next { get; set; }
+            public Entry(K key, V value)
+            {
+                this.key = key;
+                this.value = value;
+                next = null;
+            }
+        }
+        private Entry[] table;
+        int size = 0;
+        double loadFactor;
+        public MyHashMap(int initialCapacity, double loadFactor)
+        {
+            table = new Entry[initialCapacity];
+            this.loadFactor = loadFactor;
+        }
+        public MyHashMap() : this(16, 0.75) { }
+        public MyHashMap(int initialCapacity) : this(initialCapacity, 0.75) { }
+        public void Clear()
+        {
+            Array.Clear(table);
+            size = 0;
+        }
+        public int GetHashCode(K element) => element!.GetHashCode();
+        public int GetHashCode(V element) => element!.GetHashCode();
+        public bool ContainsKey(K key)
+        {
+            int index = Math.Abs(GetHashCode(key)) % table.Length;
+            Entry entry = table[index];
+            while(entry != null)
+            {
+                if (entry.key!.Equals(key)) return true;
+                entry = entry.next!;
+            }
+            return false;
+        }
+        public bool ContainsValue(V value)
+        {
+            foreach (var element in table)
+            {
+                for (var step = element; step != null; step = step.next)
+                    if (step.value!.Equals(value))return true;
+            }
+            return false;
+        }
+        public V? Get(K key)
+        {
+            int index = Math.Abs(GetHashCode(key)) % table.Length;
+            Entry entry = table[index];
+            while (entry != null)
+            {
+                if (entry.key!.Equals(key)) return entry.value;
+                entry = entry.next!;
+            }
+            return default;
+        }
+        public bool IsEmpty() => size == 0;
+        public K[] keySet()
+        {
+            K[] array = new K[size];
+            int index = 0;
+            foreach (var element in table)
+            {
+                for (var step = element; step != null; step = step.next)
+                    array[index++] = step.key;
+            }
+            return array;
+        }
+        public int Size() => size;
+        public void Put(K key, V value) 
+        {
+            if (size / (float)table.Length >= loadFactor) 
+                Resize();
+            Entry pair = new Entry(key, value);
+            int index = Math.Abs(GetHashCode(key)) % table.Length;
+            Entry entry = table[index];
+            if (entry == null) {
+                table[index] = pair;
+                size++;
+                return;
+            }
+            while(entry != null)
+            {
+                if (GetHashCode(entry.key).Equals(GetHashCode(key)))
+                    if (entry.key!.Equals(key)) {
+                        entry.value = value;
+                        return;
+                    }
+                if (entry.next == null)
+                {
+                    entry.next = pair;
+                    size++;
+                    return;
+                }
+                entry = entry.next!;
+            }
+        }
+        public void Resize()
+        {
+            Entry[] newTable = new Entry[table.Length * 2 + 1];
+            foreach (var element in table)
+            {
+                for (var step = element; step != null; step = step.next)
+                {
+                    Entry pair = new Entry(step.key, step.value);
+                    int index = Math.Abs(GetHashCode(pair.key)) % newTable.Length;
+                    Entry entry = newTable[index];
+                    if (entry == null) newTable[index] = pair;
+                    while (entry != null)
+                    {
+                        if (entry.next == null)
+                        {
+                            entry.next = pair;
+                            return;
+                        }
+                        entry = entry.next;
+                    }
+                }
+            }
+            table = newTable;
+        }
+        public void Remove(K key)
+        {
+            int index = Math.Abs(GetHashCode(key)) % table.Length;
+            Entry entry = table[index];
+            if (entry == null) throw new ArgumentException("Данного ключа не обнаружено");
+            if (entry.key!.Equals(key))
+            {
+                table[index] = entry.next!;
+                size--;
+                return;
+            }
+            while (entry.next != null)
+            {
+                if (entry.next.key!.Equals(key))
+                {
+                    entry.next = entry.next.next;
+                    size--;
+                    return;
+                }
+                entry = entry.next;
+            }
+        }
+        public KeyValuePair<K, V>[] EntrySet()
+        {
+            KeyValuePair<K, V>[] array = new KeyValuePair<K, V>[size];
+            int index = 0;
+            foreach (var element in table)
+            {
+                for (var step = element; step != null; step = step.next)
+                    array[index++] = new KeyValuePair<K, V>(step.key, step.value);
+            }
+            return array;
+        }
+        public void Print()
+        {
+            int index = 0;
+            foreach (var element in table)
+            {
+                Console.WriteLine($"[{index++}]:");
+                for (var step = element; step != null; step = step.next)
+                    Console.WriteLine($"\t <{step.key}, {step.value}>");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ KASD-Lab#20/KASD-Lab#20/Program.cs
@@ -0,0 +1,44 @@
+﻿using MyHashMapLib;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    enum TypeEum
+    {
+        Integer,
+        Double,
+        Float
+    }
+    public static void Main(string[] args)
+    {
+        try
+        {
+            StreamReader input = new StreamReader("input.txt");
+            MyHashMap<string, string> definitions = new MyHashMap<string, string>();
+            Regex regex = new Regex(@"(double|int|float) \S* ?(?:=) ?(\S)+?(?=;)");
+            string? line = input.ReadLine();
+            if (line == null) Console.WriteLine("Файл пуст");
+            while (line != null)
+            {
+                MatchCollection matches = regex.Matches(line);
+                foreach (Match match in matches)
+                {
+                    string[] parts = match.Value.Split(' ');
+                    string type = parts[0];
+                    string valuable = parts[3];
+                    string name = parts[1];
+                    string tv = type + " " + valuable;
+                    if (definitions.ContainsKey(name)) Console.WriteLine($"повтор: {type} {name} = {valuable}");
+                    else definitions.Put(name, tv);
+                }
+                line = input.ReadLine();
+            }
+            Console.WriteLine();
+            input.Close();
+            var pair = definitions.EntrySet();
+            foreach (var element in pair)
+                Console.WriteLine(element.Key + " " + element.Value);
+        }
+        catch (Exception ex) { Console.WriteLine("Exception : " + ex.Message); }
+    }
+}
