ff5d766e0b6b57766f512f377c21fd2a2b9bb18e
Max <kefirka552@gmail.com>
2024-12-09 20:00:45 +0300

    LabWork-#19

---
+++ KASD-Lab#19/KASD-Lab#19/Program.cs
@@ -0,0 +1,27 @@
+﻿using MyHashMapLib;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        StreamReader input = new StreamReader("input.txt");
+        MyHashMap<string, int> tags = new MyHashMap<string, int>();
+        Regex regex = new Regex(@"</?[a-zA-Z]\w*>");
+        string? line = input.ReadLine();
+        if (line == null) Console.WriteLine("Строка пуста");
+        while (line != null)
+        {
+            MatchCollection matches = regex.Matches(line);
+            foreach (Match match in matches)
+            {
+                if (!tags.ContainsKey(match.Value.ToLower())) tags.Put(match.Value.ToLower(), 1);
+                else tags.Put(match.Value.ToLower(), tags.Get(match.Value.ToLower()) + 1);
+            }
+            line = input.ReadLine();
+        }
+        input.Close();
+        KeyValuePair<string, int>[] pairs = tags.EntrySet();
+        for (int i = 0; i < pairs.Length; i++) Console.WriteLine($"{pairs[i].Key}: {pairs[i].Value}");
+
+    }
+}
