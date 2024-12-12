30775b943fa925a4df7d93497d42040752ccb6e3
Max <kefirka552@gmail.com>
2024-12-12 13:57:18 +0300

    LabWork-#21

---
+++ KASD-Lab#21/KASD-Lab#21/MyStack.cs
@@ -0,0 +1,43 @@
+﻿using MyVectorLib;
+public class Project
+{
+    public class MyStack<T> : MyVector<T>
+    {
+        private MyVector<T> stack;
+        private int top;
+        public MyStack()
+        {
+            top = -1;
+            stack = new MyVector<T>(1);
+        }
+        public void Push(T element)
+        {
+            stack.Add(element);
+            top++;
+        }
+        public T Pop()
+        {
+            T element = stack.Get(top);
+            stack.RemoveByIndex(top);
+            top--;
+            return element;
+        }
+        public T Peek()
+        {
+            return stack.Get(top);
+        }
+        public new bool IsEmpty()
+        {
+            return stack.IsEmpty();
+        }
+        public int Search(T item)
+        {
+            if (stack.IndexOf(item) == -1) return -1;
+            return top - stack.IndexOf(item) + 1;
+        }
+        public new void Print()
+        {
+            stack.Print();
+        }
+    }
+}
---
+++ KASD-Lab#21/KASD-Lab#21/TestAll.cs
@@ -0,0 +1,21 @@
+﻿using MyTreeMapLib;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MyTreeMap<int, int> tree = new MyTreeMap<int, int>();
+        tree.Put(8, 20);
+        tree.Put(3, 30);
+        tree.Put(10, 40);
+        tree.Put(1, 50);
+        tree.Put(6, 60);
+        tree.Put(14, 70);
+        tree.Put(4, 80);
+        tree.Put(7, 90);
+        tree.Put(13, 100);
+        tree.Print();
+        tree.Remove(8);
+        tree.Print();
+        MyTreeMap<int, int> tree2 = tree.TailMap(7);
+    }
+}
