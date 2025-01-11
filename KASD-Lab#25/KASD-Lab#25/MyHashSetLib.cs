63fcbdcef286611082d25251667ee81346ee5979
Max <kefirka552@gmail.com>
2025-01-11 22:23:47 +0300

    LabWork-#25

---
+++ KASD-Lab#25/KASD-Lab#25/MyHashSetLib.cs
@@ -0,0 +1,53 @@
+﻿using MyHashMapLib;
+namespace MyHashSetLib
+{
+    public class MyHashSet<T> where T: IComparable
+    {
+        MyHashMap<T, object> map;
+        public MyHashSet() => map = new MyHashMap<T, object>();
+        public MyHashSet(T[] array)
+        {
+            map = new MyHashMap<T, object>(array.Length);
+            foreach(T element in array) map.Put(element, true);
+        }
+        public MyHashSet(int initialCapacity, float loadFactor) => map = new MyHashMap<T, object>(initialCapacity, loadFactor);
+        public MyHashSet(int initialCapacity) => map = new MyHashMap<T, object>(initialCapacity);
+        public void Add(T item) => map.Put(item, true);
+        public void AddAll(T[] array)
+        {
+            foreach (T element in array) map.Put(element, true);
+        }
+        public void Clear() => map.Clear();
+        public bool Contains(T element) => map.ContainsKey(element);
+        public bool ContainsAll(T[] array)
+        {
+            foreach (T element in array) if (!map.ContainsKey(element)) return false;
+            return true;
+        }
+        public bool IsEmpty() => map.IsEmpty();
+        public void Remove(T element) => map.Remove(element);
+        public void RemoveAll(T[] array)
+        {
+            foreach(T element in array) map.Remove(element);
+        }
+        public void RetainAll(T[] array)
+        {
+            T[] keys = map.keySet();
+            foreach(T element in keys) if (!array.Contains(element)) map.Remove(element);
+        }
+        public int Size() => map.Size();
+        public T[] ToArray() => map.keySet();
+        public T[] ToArray(ref T[] array)
+        {
+            if (array == null) return ToArray();
+            return array = ToArray();
+
+        }
+        public void Print()
+        {
+            T[] table = ToArray();
+            foreach (var element in table) Console.WriteLine(element);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ KASD-Lab#25/KASD-Lab#25/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyHashSetLib;
+class Program
+{
+    class MyStringComparer: IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+            return x.Length.CompareTo(y.Length);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        MyHashSet<string> text = new MyHashSet<string>();
+        StreamReader input = new StreamReader("input.txt");
+        string? line = input.ReadLine();
+        while (line != null)
+        {
+            string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words) text.Add(word);
+            line = input.ReadLine();
+        }
+        string[] outWords = text.ToArray();
+        Array.Sort(outWords, new MyStringComparer());
+        foreach (string word in outWords) Console.WriteLine(word);
+    }
+}
