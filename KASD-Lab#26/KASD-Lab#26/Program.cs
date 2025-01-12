166a843f3bf0d6d449e37540cffb2033e6207611
Max <kefirka552@gmail.com>
2025-01-12 12:27:10 +0300

    LabWork-#26

---
+++ KASD-Lab#26/KASD-Lab#26/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using MyHashSetLib;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MyHashSet<string> text = new MyHashSet<string>();
+        StreamReader input = new StreamReader("input.txt");
+        string? line = input.ReadLine();
+        string pattern = @"\b[a-zA-Z]+\b";
+        while (line != null)
+        {
+            MatchCollection matches = Regex.Matches(line, pattern);
+            foreach (Match match in matches) text.Add(match.Value.ToLower());
+            line = input.ReadLine();
+        }
+        input.Close();
+        Console.WriteLine("Уникальные слова");
+        text.Print();
+    }
+}
